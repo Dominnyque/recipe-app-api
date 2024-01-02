e963762ccadf5309dbc82452e147ad9e559dca01
Dominique <59777085+Dominnyque@users.noreply.github.com>
2024-01-02 16:20:28 +0200

    Corrected flake8 tests failure5

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -4,6 +4,8 @@ from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
+
+
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
     """test command"""
@@ -12,11 +14,12 @@ class CommandTests(SimpleTestCase):
         patched_check.return_value = True
         call_command('wait_for_db')
         patched_check.assert_called_once_with(databases=['default'])
+        
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
         """Test waiting for database when getting operational error"""
         patched_check.side_effect = [Psycopg2Error] * 2 + \
-        [OperationalError] * 3 + [True]
+         [OperationalError] * 3 + [True]
         call_command('wait_for_db')
         self.assertEqual(patched_check.call_count, 6)
         patched_check.assert_called_with(databases=['default'])
